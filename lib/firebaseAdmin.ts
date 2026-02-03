6897b92cd2fc20c9d3f3a05f03153e2bcc45ea5c
Germán <gcpmendez@gmail.com>
2026-02-03 23:41:14 +0000

    3 commit

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -2,8 +2,19 @@ import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
     try {
+        let credential;
+        if (process.env.FIREBASE_PRIVATE_KEY) {
+            credential = admin.credential.cert({
+                projectId: process.env.FIREBASE_PROJECT_ID,
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+                privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+            });
+        } else {
+            credential = admin.credential.cert(require('../service-account.json'));
+        }
+
         admin.initializeApp({
-            credential: admin.credential.cert(require('../service-account.json')),
+            credential,
         });
     } catch (error) {
         console.error('Firebase admin initialization error', error);
